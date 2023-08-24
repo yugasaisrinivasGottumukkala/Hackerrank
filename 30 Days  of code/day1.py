5b608b6c75ab2e7f9631b3b4263b20e70d28e1fa
yuga sai srinivasu gottumukkala <36449483+yugasaisrinivasGottumukkala@users.noreply.github.com>
2023-08-24 23:32:56 +0530

    Create day1.py
    Day -1 of Hackerrank 30 days of code

---
+++ 30 Days  of code/day1.py
@@ -0,0 +1,17 @@
+#Day-1 of Hackerrank 30 days of code
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+Si=int(input())
+Sd=float(input())
+Ss=input()
+# Print the sum of both integer variables on a new line.
+print(i+Si)
+# Print the sum of the double variables on a new line.
+print(d+Sd)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+Ss)
